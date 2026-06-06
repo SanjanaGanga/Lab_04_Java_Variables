1ba084312d5a1ac6725eb8cdeac244e4307ff424
Sanjana Ganga <gangasc@mail.uc.edu>
2026-06-05 22:28:56 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main() {
+        //Declare Integer variables
+        int intOperandA = 18;
+        int intOperandB = 3;
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        //declare Double variables
+        double doubleOperandA = 17.50;
+        double doubleOperandB = 4.50;
+        double doubleSum = 0.00;
+        double doubleProduct = 0.00;
+        double doubleDifference = 0.00;
+        double doubleQuotient = 0.00;
+
+        double myLunchCost =  12.50;
+        int numberOfKidsInFamily = 5;
+        boolean isItRaining = false;
+        double priceOfGallonGas = 3.99;
+        int favNumber = 5;
+        double shoeSize = 8.5;
+        String birthMonth = "December";
+        String fullName = "Sanjana Ganga";
+
+        intSum = intOperandA + intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum); // Output statement
+
+        intProduct = intOperandA * intOperandB; // Assignment that uses the arithmetic operator -
+        System.out.println("The product using ints of " + intOperandA + " and  " + intOperandB + " is " + intProduct); // Output statement
+
+        intDifference = intOperandA - intOperandB; // Assignment that uses the arithmetic operator -
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference); // Output statement
+
+        intQuotient = intOperandA / intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient); // Output statement
+
+        intModulo = intOperandA % intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The modulo using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo); // Output statement
+
+        doubleSum = doubleOperandA + doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum); // Output statement
+
+        doubleProduct = doubleOperandA * doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The product using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct); // Output statement
+
+        doubleDifference = doubleOperandA - doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The difference using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference); // Output statement
+
+        doubleQuotient = doubleOperandA / doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The quotient using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient); // Output statement
+    }
 }
